307a59167daf5a1b2f6a34bf1ff3274cc1158c8b
Jabborov Elbek <jabborovelbek345@gmail.com>
2026-05-14 15:35:43 +0300

    Update dars.py

--- dars.py
+++ dars.py
@@ -1,1 +1,33 @@
+# from uuid import uuid4
 
+# class shaxs:
+#     foydalanganlik_soni = 0
+#     def __init__(self,ism,familiya,tyil,jshshr):
+#         self.name = ism
+#         self.surname = familiya
+#         self.born = tyil
+#         self.__id = jshshr
+#         shaxs.foydalanganlik_soni += 1
+#     def get_jshshr(self):
+#         return f"shaxsning jashshr : {self.__id}"
+#     def get_name(self):
+#         return self.name.title()
+#     def get_surname(self):
+#         return self.surname.title()
+#     def get_tugilgan_yil(self):
+#         return self.born
+#     def get_age(self):
+#         return f"{2026 - self.born} - yoshda"
+#     def get_class_foydalanuvchilar(cls):
+#         return cls.foydalanganlik_soni
+
+# t1 = shaxs('kamolyoy', 'toymurodov', 2027, 465780423376666)
+# t2 = shaxs('kamolla', 'murodova', 20305, 754708635668)
+# t3 = shaxs('kamolyot', 'toymurodova', 2127, 46578423376666)
+# t4 = shaxs('kamoblyoy', 'toymurohdov', 20527, 4657804237666)
+# t5 = shaxs('kamlyoy', 'toymjurodov', 5027, 4657042337666)
+# t6 = shaxs('kamogyoy', 'toymurrdov', 227, 4657802337666)
+# t7 = shaxs('kamylyoy', 'toymurobdov', 20027, 4578042336666)
+# t8= shaxs('kamolsyoy', 'toyxmurodov', 2027, 46570423376666)
+# t9 = shaxs('kamoltyoy', 'toymur6odov', 2027, 46578042376666)
+# t10 = shaxs('kadmolyoy', 'toymuroodov', 2027, 46578042376666)
